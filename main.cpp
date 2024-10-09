380825076e10596a7d2b88b177baad11b5fa1700
VarshiniGowdaV <varshinigowdav14@gmail.com>
2024-10-09 12:15:02 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int main()
+{
+int a, b, temp;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Swapping using a temporary variable
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
